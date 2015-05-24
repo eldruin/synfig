ccbf953f273822c89b82476a47e8953661855345
Diego Barrios Romero <eldruin@eldruin.com>
2015-05-24 20:49:15 +0200

    Add tests for color constructors and component getters and setters

--- synfig-core/test/gtest/color.cpp
+++ synfig-core/test/gtest/color.cpp
@@ -29,10 +29,78 @@
 
   #include <gtest/gtest.h>
   #include <synfig/color/color.h>
+  #include <boost/type_traits/is_floating_point.hpp>
+  #include <boost/type_traits/is_integral.hpp>
+  #include <boost/utility/enable_if.hpp>
 #endif
 
-TEST(Color, CanCreateColor)
+using synfig::Color;
+typedef Color::value_type color_type;
+
+class ColorTest : public testing::Test
 {
-  synfig::Color c;
+protected:
+  ColorTest()
+    : r(1.3),  g(1.4),  b(1.5),  a(1.6),
+      r2(2.3), g2(2.4), b2(2.5), a2(2.6)
+  { }
+
+  static void RGBA_EQ(const Color& current, const float r, const float g,
+                                            const float b, const float a)
+  {
+    ASSERT_FLOAT_EQ(r, current.get_r());
+    ASSERT_FLOAT_EQ(g, current.get_g());
+    ASSERT_FLOAT_EQ(b, current.get_b());
+    ASSERT_FLOAT_EQ(a, current.get_a());
+  }
+
+  const color_type r,  g,  b,  a,
+                   r2, g2, b2, a2;
+};
+
+
+
+TEST_F(ColorTest, ByDefaultAll0)
+{
+  RGBA_EQ(Color(), 0, 0, 0, 0);
 }
+
+TEST_F(ColorTest, 1ValueConstructor)
+{
+  const color_type c(1.3);
+  RGBA_EQ(Color(c), c, c, c, c);
+}
+
+TEST_F(ColorTest, 4ValueConstructor)
+{
+  RGBA_EQ(Color(r, g, b, a), r, g, b, a);
+}
+
+TEST_F(ColorTest, 3ValueConstructorAlphaIsDefault)
+{
+  RGBA_EQ(Color(r, g, b), r, g, b, 1.0f);
+}
+
+TEST_F(ColorTest, CopyConstructor)
+{
+  Color c(r, g, b, a);
+  RGBA_EQ(Color(c), r, g, b, a);
+}
+
+TEST_F(ColorTest, CopyColorComponentsAndSeparateAlpha)
+{
+  Color c(r, g, b, a);
+  RGBA_EQ(Color(c, a2), r, g, b, a2);
+}
+
+TEST_F(ColorTest, SetRGBA)
+{
+  Color c(r, g, b, a);
+  c.set_r(r2);
+  c.set_g(g2);
+  c.set_b(b2);
+  c.set_a(a2);
+  RGBA_EQ(c, r2, g2, b2, a2);
+}
+
 
